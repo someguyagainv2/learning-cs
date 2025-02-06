448e270f893a586599b03a01d969fd9924bdec05
someguyagain <145267660+someguyagainv2@users.noreply.github.com>
2025-02-06 19:11:33 +0000

    Create LoopingThroughAnArray.cs

---
+++ code/LoopingThroughAnArray.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace SoloLearn
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] list = {1, 3, 5, 9, 12};
+			for (int x = 0; x <= 4; x++)
+			{
+				Console.WriteLine(list[x]);
+			}
+
+		}
+	}
+}
