e26d1feccab0a317d6f3781ab1796537e192e737
someguyagain <145267660+someguyagainv2@users.noreply.github.com>
2025-02-06 17:53:56 +0000

    Create BasicElseIFStatements.cs

---
+++ code/BasicElseIFStatements.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace SoloLearn
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int x = 5, y = 3;
+			int z = 10, b = 10;
+
+			if (x == y) {
+				Console.WriteLine("Numbers X and Y are equal!");
+			}
+			else if (x > y) {
+				Console.WriteLine("X is bigger than Y");
+			}
+
+			if (z == b) {
+				Console.WriteLine("Numbers Z and B are equal!");
+			};
+		}
+	}
+}
