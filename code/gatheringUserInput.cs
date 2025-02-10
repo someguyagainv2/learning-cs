aa92f2e3ce740adb6b33f6f5b49fc2cbf44f11a2
someguyagain <145267660+someguyagainv2@users.noreply.github.com>
2025-02-10 12:01:32 +0000

    Create gatheringUserInput.cs

---
+++ code/gatheringUserInput.cs
@@ -0,0 +1,8 @@
+string username = Console.ReadLine();
+string password = Console.ReadLine();
+
+if (username == "someguyagain" && password == "someguyagainV2") {
+  Console.WriteLine("Logged In");
+} else {
+  Console.WriteLine("Failed log in")
+}
