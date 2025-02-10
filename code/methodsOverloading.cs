77fb9923f08021bae91b9ce283c13d3900f51561
someguyagain <145267660+someguyagainv2@users.noreply.github.com>
2025-02-10 12:06:51 +0000

    Update methodsOverloading.cs

--- code/methodsOverloading.cs
+++ code/methodsOverloading.cs
@@ -1,1 +1,13 @@
+static int AddValues(int x, int y) {
+  return x+y;
+}
 
+static double AddValues(double x, double y) {
+  return x+y;
+}
+
+int returnOverloadedFirst = AddValues(1,3);
+double returnOverloadedSecond = AddValues(5.09, 2.01);
+
+Console.WriteLine(returnOverloadedFirst);
+Console.WriteLine(returnOverloadedSecond);
